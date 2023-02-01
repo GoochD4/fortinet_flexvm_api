5530db0b954a3213f305aa6c0f8528589f21ff05
GoochD4 <31257516+GoochD4@users.noreply.github.com>
2023-02-01 10:16:00 -0600

    Create flexvm_create_entitlement.py

---
+++ flexvm_create_entitlement.py
@@ -0,0 +1,87 @@
+#!C:\\Users\\dgooch\\AppData\\Local\\Programs\\Python\\Python311
+ 
+import requests, json
+
+##added these lines to prevent the warning for insecure request
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def fauth():
+    ####This first section pulls the OAUTH token from fortinet
+    fauth_url = "https://customerapiauth.fortinet.com/api/v1/oauth/token/"
+    headers = {'Content-Type' : 'application/json; charset=utf-8'}
+    body = {
+        "username": "<appID>",
+        "password": "<password>",
+        "client_id": "flexvm",
+        "grant_type": "password"
+    } 
+
+
+    ##This opens the api request and prints the entire thing
+    try: 
+        response = requests.post(
+        url=fauth_url,
+        headers=headers,
+        json=body,
+        # verify=False
+        )
+        jsonresponse = response.json()
+    # print("Entire JSON response")
+        # print(jsonresponse)
+
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+    ##This code assigns a variable to the access_token dictionary
+    token = jsonresponse['access_token']
+    return token
+    # print(token)
+
+
+toktype = 'Bearer '
+otoken = fauth()
+fin_tok = toktype+otoken
+
+
+### This section will use the token to create an entitlement
+def vmcreate():
+    flex_url = " https://support.fortinet.com/ES/api/flexvm/v1/vms/create"
+    headers = {'Authorization': fin_tok}
+    body = {
+        "configId": 741,
+        "count": 1,
+        "description": "test-vm api post",
+        "endDate": "2023-2-5 00:00:00",
+        "folderPath": "My Assets"
+    } 
+
+    try: 
+        response = requests.post(
+        url=flex_url,
+        headers=headers,
+        json=body,
+        # verify=False
+        )
+        jsonresponse = response.json()
+    # print("Entire JSON response")
+        # print(jsonresponse)
+
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+    # ##This line prints the entire output of the request results in a nice readable format
+    # print(json.dumps(jsonresponse, indent=4, sort_keys=True))
+
+    json_results = jsonresponse['vms']
+
+
+    ##this looks for the ip_mask value.  If it is 
+    for value in json_results:
+        if value['token']:
+            # print (value['token'])
+            return value['token'] 
+
+flextoken = vmcreate()
+# print(flextoken)
